1a06b5fe7b80db7bdd3b26b6f6e7c810ee70d5dd
GuoDaiYi <GuoDaiYi.me@icloud.com>
2026-09-07 12:36:27 +0000

    提交注释

--- .vscode/Basic.c
+++ .vscode/Basic.c
@@ -1,3 +1,3 @@
 int main(){
-    return 0;
+    return 0;//返回运行结束结果
 }
--- .vscode/print_course3.c
+++ .vscode/print_course3.c
@@ -10,3 +10,9 @@ int main()
     printf("John Score=%d\n",john_score);
     printf("Mike Score=%d\n",mike_score);
 }
+//题目：已知 Mike 成绩是 98，John 成绩是 89，可在登记成绩时因看错行，导致 Mike 的成绩被错误登记在 John 那里，现在要把两位的成绩纠正过来。
+//请完成以下练习：
+//1、创建两个整型变量mike_score和john_score,分别初始化为89和98。
+//2、将John的成绩赋值给Mike
+//3、将john的成绩更改为89
+//4、打印出两个人的成绩
