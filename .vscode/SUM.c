344ac3f19e0f41c37bd152ba81e24a901dcc0fd8
GuoDaiYi <GuoDaiYi.me@icloud.com>
2026-09-09 02:56:52 +0000

    更新注释

--- .vscode/SUM.c
+++ .vscode/SUM.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    int a,b,sum;
-    a=120,b=32,sum=0;
-    sum=a+b;
-    printf("sum is %d\n",sum);
+    int a,b,sum;//设置函数类型
+    a=120,b=32,sum=0;//设置函数数值
+    sum=a+b;//相加
+    printf("sum is %d\n",sum);//输出
     return 0;
 }
