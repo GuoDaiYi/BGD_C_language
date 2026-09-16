c2e3aedc5050f7e4233811323d04bcd4c5ca656f
GuoDaiYi <GuoDaiYi.me@icloud.com>
2026-09-16 04:05:19 +0000

    updaten hello

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+char c1='H',c2='E',c3='L',c4='O',c5='C';
+printf("%c%c%c%c%c %c\n",c1,c2,c3,c3,c4,c5);
+printf("%d%d%d%d%d %d\n",c1,c2,c3,c3,c4,c5);
+int sum=c1+c2+c3+c4+c5;
+printf("%d\n",sum);
+}
